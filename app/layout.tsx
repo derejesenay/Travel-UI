73ace7b7a1e593fc8eddf6b7177b6ad78a097d68
Dereje Senay <dereje.467.467@gmail.com>
2024-04-08 15:22:55 +0300

    First Commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        <main className="relative overflow-hidden">{children}</main>
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
